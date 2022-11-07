fd0ea5dd6fc4111e23e9e2ba09bae63f043cd40b
Cside <cside.story@gmail.com>
2022-11-07 13:55:02 +0900

    modify test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,64 +1,63 @@
 import { debounce, throttle } from './index';
 
-for (const obj of [
+describe.each([
   { name: 'throttle', func: throttle },
   { name: 'debounce', func: debounce },
-]) {
-  const { name, func } = obj;
-  describe(name, () => {
-    describe('basic', () => {
-      beforeAll(() => jest.useFakeTimers());
-      afterAll(() => jest.useRealTimers());
+])('$name', ({ func }) => {
+  describe('basic', () => {
+    beforeAll(() => jest.useFakeTimers());
+    afterAll(() => jest.useRealTimers());
 
-      it('called decreased times', () => {
+    it('called decreased times', () => {
+      const mockedFn = jest.fn();
+      const fn = func(mockedFn, 0);
+      fn();
+      fn();
+
+      jest.runAllTimers();
+      expect(mockedFn).toHaveBeenCalledTimes(1);
+    });
+    it('called with args', () => {
+      for (const args of [[], ['a'], ['a', 'b']]) {
         const mockedFn = jest.fn();
         const fn = func(mockedFn, 0);
-        fn();
-        fn();
+        fn(...args);
 
         jest.runAllTimers();
-        expect(mockedFn).toHaveBeenCalledTimes(1);
-      });
-      it('called with args', () => {
-        for (const args of [[], ['a'], ['a', 'b']]) {
-          const mockedFn = jest.fn();
-          const fn = func(mockedFn, 0);
-          fn(...args);
-
-          jest.runAllTimers();
-          expect(mockedFn).toHaveBeenLastCalledWith(...args);
-        }
-      });
-    });
-    describe('resolve/reject', () => {
-      it('resolves in sync fn', () => {
-        const fn = func(() => 'resolve', 0);
-        return expect(fn()).resolves.toBe('resolve');
-      });
-      it('resolves in async fn', () => {
-        const fn = func(() => Promise.resolve('resolve'), 0);
-
-        if (name === 'throttle') {
-          return expect(Promise.all([fn(), fn()])).resolves.toEqual([
-            'resolve',
-            null,
-          ]);
-        } else {
-          // 最初の call は clearTimeout されるので resolve されない
-          fn();
-          return expect(fn()).resolves.toBe('resolve');
-        }
-      });
-      it('rejects in async fn', () => {
-        const fn = func(() => Promise.reject('reject'), 0);
-        return expect(fn()).rejects.toBe('reject');
-      });
-      it('rejects in throwing fn', () => {
-        const fn = func(() => {
-          throw 'reject';
-        }, 0);
-        return expect(fn()).rejects.toBe('reject');
-      });
+        expect(mockedFn).toHaveBeenLastCalledWith(...args);
+      }
     });
   });
-}
+  describe('resolve/reject', () => {
+    it('resolves in sync fn', () => {
+      const fn = func(() => 'resolve', 0);
+      return expect(fn()).resolves.toBe('resolve');
+    });
+    it('rejects in async fn', () => {
+      const fn = func(() => Promise.reject('reject'), 0);
+      return expect(fn()).rejects.toBe('reject');
+    });
+    it('rejects in throwing fn', () => {
+      const fn = func(() => {
+        throw 'reject';
+      }, 0);
+      return expect(fn()).rejects.toBe('reject');
+    });
+  });
+});
+
+describe('resolves in async fn', () => {
+  it('throttle', () => {
+    const fn = throttle(() => Promise.resolve('resolve'), 0);
+    return expect(Promise.all([fn(), fn()])).resolves.toEqual([
+      'resolve',
+      null,
+    ]);
+  });
+  it('debounce', () => {
+    const fn = debounce(() => Promise.resolve('resolve'), 0);
+    // 最初の call は clearTimeout されるので resolve されない
+    fn();
+    return expect(fn()).resolves.toBe('resolve');
+  });
+});
